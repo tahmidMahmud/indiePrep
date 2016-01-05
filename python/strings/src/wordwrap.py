525b5e94d425bab73b989331111a8fe614faf454
Tahmid Mahmud <tmahmud94@gmail.com>
2016-01-04 22:04:45 -0600

    branch test

--- python/strings/src/wordwrap.py
+++ python/strings/src/wordwrap.py
@@ -2,18 +2,7 @@ def wordwrap(str, width = 75, _break = "\n", cut = False):
 	if len(str) < width:
 		return str
 	else:
-		if cut ==False:
-			wordList = str.split()
-			str = ''
-			cutIndex = 0
-			for word in wordList:
-				if len(str[cutIndex:] + word) < width:
-					str = str + word + " "
-				else:
-					str = str + _break + word + " "
-					cutIndex = len(str)
-			return str[:len(str)]
-		else:
+		
 			cutIndex = width
 			for i in range(len(str)//width):
 				str = str[:cutIndex] + _break + str[cutIndex:]
