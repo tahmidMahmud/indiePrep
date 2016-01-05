84dfe23514d76b0a0023964a19bbafff4cb383a2
Tahmid Mahmud <tmahmud94@gmail.com>
2016-01-04 22:40:43 -0600

    implemented wordwrap without split()

--- python/strings/src/wordwrap.py
+++ python/strings/src/wordwrap.py
@@ -2,12 +2,17 @@ def wordwrap(str, width = 75, _break = "\n", cut = False):
 	if len(str) < width:
 		return str
 	else:
-		
-			cutIndex = width
-			for i in range(len(str)//width):
-				str = str[:cutIndex] + _break + str[cutIndex:]
-				cutIndex = cutIndex + width + len(_break) 
-			return str
+		cutIndex = width
+		for i in range(len(str)//width):
+                        if cut == False:
+                                while str[cutIndex] != ' ':
+                                        cutIndex = cutIndex - 1
+                                str = str[:cutIndex] + _break + str[cutIndex+1:]
+                        else:
+                                str = str[:cutIndex] + _break + str[cutIndex:]
+                        cutIndex = cutIndex + width + len(_break) 
+		return str
 
+                
 
 
--- python/strings/test/wordwrap.py
+++ python/strings/test/wordwrap.py
@@ -7,25 +7,27 @@ from src.wordwrap import wordwrap
 class Test(unittest.TestCase):
 	
 	def test_canary(self):
+
 		self.assertTrue(True)
 
 	def test_wordLengthLessThenCutWidth(self):
+
 		self.assertEqual(wordwrap('hello'), 'hello')
 
 	def test_wordWidth8BreakWithnewLine(self):
 		word = wordwrap('hello goodbye', 8)
-		x = word.find('\n')
-		self.assertEqual(x, 6)
+
+		self.assertEqual(word,'hello\ngoodbye')
 
 	def test_wordWidth8BreakWithDollarSymbol(self):
 		word = wordwrap('hello goodbye', 8, '$')
-		x = word.find('$')
-		self.assertEqual(x, 6)
+		
+		self.assertEqual(word, 'hello$goodbye')
 
 	def test_wordWidth8CutEqualToTrue(self):
 		word = wordwrap('hello goodbye', 8, '\n', True)
-		x = word.find('\n')
-		self.assertEqual(x, 8)
+
+		self.assertEqual(word, 'hello go\nodbye')
 
 if __name__ == '__main__':
 	unittest.main()
